07a035532f987f9251e9a0e43e22cbe5c473b6e5
Andrew Chander <andrewchander@gmail.com>
2021-10-22 13:41:06 +1100

    Removed example page

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
- import React from "react"
- import styled from "styled-components/macro"
- import tw from "tailwind.macro"
- 
- // styles
- const Header = styled.header`
-   ${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl text-white`};
- `
- 
- const Test = () => (
-   <div css={tw`text-center`}>
-     <Header>
-       <p css={tw`text-blue-300`}>
-         Using <code>Tailwind</code> and <code>styled-components</code> together.
-       </p>
-     </Header>
-   </div>
- )
- 
-
-
 export default function Home() {
   return (
     <div className={styles.container}>
